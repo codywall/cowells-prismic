b7da5ac98e7831c7940319f9ff4496a6634260a1
codywall <cody.wall@gmail.com>
2020-05-12 16:02:55 -0700

    Create form class and success page

---
+++ src/pages/contact/thanks.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+import Intro from '../../components/Intro'
+
+export default () => (
+  <Layout>
+        <Intro title="Thank you!" text="We received your inquiry, and will contact you shortly!"/>
+  </Layout>
+)
